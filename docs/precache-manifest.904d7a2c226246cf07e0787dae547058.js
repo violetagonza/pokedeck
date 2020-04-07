e6da1948438fb3d4e67f4af2bc55ce77680bc5c9
violetagonza <violetagonzalezcalleja@gmail.com>
2020-04-07 08:39:57 +0200

    Update docs

---
+++ docs/precache-manifest.904d7a2c226246cf07e0787dae547058.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a318fd8608f0900c49d8699e1d4c335",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c36991ff23f286d3ff09",
+    "url": "./static/js/2.ffc867f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.ffc867f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fcc02766c365dde736",
+    "url": "./static/js/main.20da2630.chunk.js"
+  },
+  {
+    "revision": "2b794caf4684d690675d",
+    "url": "./static/js/runtime-main.f7590b38.js"
+  }
+]);
